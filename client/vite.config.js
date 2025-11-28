c5d9da5594b90fb05ced4e73ba3ceb632e608924
ravindersingh74123 <ravindersingh74123@gmail.com>
2025-11-28 22:34:13 +0530

    fixing local storage issues for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://videocallapp-q3f0.onrender.com/',
     },
   }
 })
